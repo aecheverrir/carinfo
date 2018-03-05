6c9150002ff28f1f0f0350bc70ba0608aeb403ce
AlejandroEcheverri <a.echeverrir@uniandes.edu.co>
2018-03-05 10:32:36 -0500

    detalles results

--- client/components/Results/ResultItem.js
+++ client/components/Results/ResultItem.js
@@ -7,11 +7,11 @@ export default class ResultItem extends React.Component {
     return (
     	<div>
     		<img src={this.props.data.imagen} alt="asf"/>
-    		<h3>{this.props.data.maraca}</h3>
-    		<h3>{this.props.data.modelo}</h3>
-    		<h5>{this.props.data.linea}</h5>
-    		<h5>{this.props.data.precio}</h5>
-    		<h5>{this.props.data.pais}</h5>
+    		<h3>Marca: {this.props.data.maraca}</h3>
+    		<h3>Modelo: {this.props.data.modelo}</h3>
+    		<h5>Linea: {this.props.data.linea}</h5>
+    		<h5>Precio: ${this.props.data.precio}</h5>
+    		<h5>Pais de origen: {this.props.data.pais}</h5>
 
     		<br/>
     		<br/>
